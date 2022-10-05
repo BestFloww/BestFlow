3798db00b15b4576790b4808ad1a587e9e0c7b8b
Kaylee Chan <kayleel.chan@gmail.com>
2022-10-05 18:20:11 -0400

    Update VoiceFlowButton
    Changed console log to reflect specific data from our VoiceFlow project when pressing the button.

--- front-end/src/components/VoiceFlowButton.jsx
+++ front-end/src/components/VoiceFlowButton.jsx
@@ -5,7 +5,9 @@ import VoiceFlowAPI from "../services/VoiceFlowAPI.js"
 const VoiceFlowButton = () => {
   const callAPI = async() => {
         const response = await VoiceFlowAPI.get();
-        console.log(response.data.message);
+        console.log(response.data);
+        console.log(response.data.variables.last_utterance);
+        console.log(response.data.variables.intent_confidence);
   };
   return (
     <BaseButton 
