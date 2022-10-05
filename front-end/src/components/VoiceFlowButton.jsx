5dafb002aa6cf1fa2ff5aaa71532ce7f99d4822d
Kaylee Chan <82854864+KayleeLChan@users.noreply.github.com>
2022-10-04 21:42:51 -0400

    Update front-end/src/components/VoiceFlowButton.jsx
    Co-authored-by: Samuel Weiss <76976842+WeissSa@users.noreply.github.com>

--- front-end/src/components/VoiceFlowButton.jsx
+++ front-end/src/components/VoiceFlowButton.jsx
@@ -3,11 +3,15 @@ import BaseButton from './BaseButton.jsx';
 import VoiceFlowAPI from "../services/VoiceFlowAPI.js"
 
 const VoiceFlowButton = () => {
-  return (
-    <BaseButton click={async() => {
+  const callAPI = async() => {
         const response = await VoiceFlowAPI.get();
         console.log(response.data.message);
-    }}text='Call the VoiceFlow API'/>
+  };
+  return (
+    <BaseButton 
+    click={callAPI}
+    text='Call the VoiceFlow API'
+    />
   )
 };
 
