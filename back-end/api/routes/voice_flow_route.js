93c5d035863f1c2c6471e9e87a3a13a4729d201b
Helia <heliasajjadian@gmail.com>
2022-10-05 20:47:22 -0400

    deleted_comments

--- back-end/api/routes/voice_flow_route.js
+++ back-end/api/routes/voice_flow_route.js
@@ -22,21 +22,6 @@ async function getTranscript(req, res) {
   res.json(response.data);
 }
 
-//const api_key = "VF.DM.6331204c9575ca00085c3fee.3xaArTcugE4obpnp"
-//const version = "632b79c564484143a984b02e"
-//async function getTranscript(req, res) {
-//  const response = await axios({
-//    method: 'get',
-//    baseURL: 'https://api-dm-test.voiceflow.fr',
-//    url: `/exportraw/${api_key}?versionID=${version}`,
-//    headers: {
-//      Authorization: api_key,
-//    },
-//    data: {},
-//  });
-//  res.json(response.data);
-//}
-
 router.route("/").get(getTranscript);
 
 export default router;
