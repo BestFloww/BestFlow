d335d952bcf9d3399171779445b36521213a2bec
WeissSa <WeissBsamuel@gmail.com>
2022-10-07 19:06:56 -0400

    fixed redux + started looping demo

--- front-end/src/components/VoiceFlowButton.jsx
+++ front-end/src/components/VoiceFlowButton.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
 import BaseButton from './BaseButton.jsx';
 import VoiceFlowAPI from "../services/VoiceFlowAPI.js"
+import React, { Component } from 'react'
 
-const VoiceFlowButton = () => {
-  const callAPI = async() => {
-        const response = await VoiceFlowAPI.get();
-        console.log(response.data);
-        console.log(response.data.variables.last_utterance);
-        console.log(response.data.variables.intent_confidence);
-  };
-  return (
-    <BaseButton 
-    click={callAPI}
-    text='Call the VoiceFlow API'
-    />
-  )
-};
+class VoiceFlowButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lastUtterance: "Call Voiceflow",
+      resultObject: null,
+    };
+    this.callVoiceFlow = this.callVoiceFlow.bind(this);
+  }
+
+  async callVoiceFlow() {
+    const result = await VoiceFlowAPI.get();
+    console.log(result);
+    this.setState({
+      lastUtterance: result.data.variables.last_utterance,
+      resultObject: result.data.variables,
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <BaseButton
+          text={this.state.lastUtterance}
+          click={this.callVoiceFlow}
+        />
+        {this.state.resultObject && Object.keys(this.state.resultObject).forEach((key) => {
+          return (
+            <h3>
+              {key} {this.state.resultObject[key]}
+            </h3>
+          )
+        }
+      )}
+      </div>
+    )
+  }
+}
+
 
 export default VoiceFlowButton;
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './store/counters/counterSlice.js';
 
 export default configureStore({
-  reducer: {}
+  reducer: {counterReducer}
 });
