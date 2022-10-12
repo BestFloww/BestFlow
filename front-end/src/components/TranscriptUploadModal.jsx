c4c3568b95bd05dc4cc91b760904cbdb91556515
Kaylee Chan <kayleel.chan@gmail.com>
2022-10-12 10:27:13 -0400

    Added UploadTranscriptModal
    Still some kinks to work out, the close button doesn't actually have the closing functionality and the upload button needs to connect to our backend to upload into our database.

--- front-end/src/components/TranscriptUploadModal.jsx
+++ front-end/src/components/TranscriptUploadModal.jsx
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import "./TranscriptUploadModal.css";
+import axios from 'axios';
 
 class TranscriptUploadModal extends Component {
+
+  state = {
+    file: null
+  }
+
+  handleFile(e) {
+    console.log(e.target.files, "File Uploaded");
+    console.log(e.target.files[0], "First Object of File Upload");
+  }
+
+  handleUpload(e) {
+    let file=this.state.file
+    let formData= new FormData()
+
+    formData.append('transcript', file)
+
+    axios({
+      url: '/some/api',
+      method: "PUT",
+      headers:{
+        authorization: 'token'
+      },
+      data: formData
+    })
+  }
+
   render() {
     if (!this.props.show) {
       return null;
     }
-    return <div>Transcript Upload Modal placeholder</div>;
+    return <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseButton">
+          <button> x </button>
+        </div>
+        <div className="title">
+          <h1>
+            Please upload your transcript below.
+          </h1>
+        </div>
+        <div className="body">
+          <div className="fileSelection">
+            <input type="file" name="file" onChange={(e)=>this.handleFile(e)}></input>
+          </div>
+
+          <br />
+          <button type="button" onClick={(e)=>this.handleUpload(e)}> Upload </button>
+        </div>
+      </div>
+    </div>;
   }
 }
 
