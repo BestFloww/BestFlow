f2bd89f82ffcac37122432ab19b85aefa72f8cf1
Kaylee Chan <kayleel.chan@gmail.com>
2022-10-19 21:42:05 -0400

    Cleaned up modal

--- front-end/src/components/TranscriptUploadModal.jsx
+++ front-end/src/components/TranscriptUploadModal.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 import Modal from 'react-modal';
+//import TranscriptAPI from "./services/TranscriptAPI";
 
 class TranscriptUploadModal extends React.Component {
 
@@ -17,19 +18,11 @@ class TranscriptUploadModal extends React.Component {
   }
 
   handleUpload(e) {
-    let file=this.state.file
-    let formData= new FormData()
+    const file=this.state.file;
+    const formData= new FormData();
 
     formData.append('transcript', file)
-
-    axios({
-      url: '/some/api',
-      method: "PUT",
-      headers:{
-        authorization: 'token'
-      },
-      data: formData
-    })
+    //TranscriptAPI.post({data:formData})
   }
 
   componentDidMount() {
@@ -40,49 +33,25 @@ class TranscriptUploadModal extends React.Component {
     return (
       <Modal
         isOpen={this.props.show}
-        className="reactModal bg-purple-200"
+        className="container mx-auto bg-purple-200 rounded-lg shadow-lg py-3 m-12"
         onRequestClose={this.props.toggleModal}
         shouldCloseOnEsc={true}
-        style={{overlay: {
-          position:'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          backgroundColor: 'rgba(255, 255, 255, 0.75)'
-        }, content: {
-          position: 'fixed',
-          justifyContent: 'center',
-          top: '35vh',
-          left: '35vw',
-          right: '35vw',
-          bottom: '35vh',
-          border: '1px solid #ccc',
-          overflow: 'auto',
-          WebkitOverflowScrolling: 'touch',
-          borderRadius: '4px',
-          outline: 'none',
-          padding: '20px'
-        }
-      }}
       >
-        <h2 className="m-3 justify-center flex">{"Drag and drop file or upload below."}</h2>
-          <div className={"modalContainerVertical justify-center flex"}>
-            <div className={"modalContainer m-3"}>
+        <h2 className="justify-center flex m-3">
+          Drag and drop file or upload below.
+        </h2>
+          <div className={"justify-center flex m-7"}>
               <input
                 type="file"
                 name="new_file"
                 onChange={(e)=>this.handleFile(e)}>
               </input>
-            </div>
-            <div className={"modalContainer"}>
               <button
                 type="button"
-                className="bg-purple-300 rounded-lg shadow-lg hover:bg-purple-200 active:bg-purple-400 m-3 py-1 px-2"
+                className="bg-gray-100 rounded-lg shadow-lg hover:bg-gray-200 active:bg-gray-300 py-1 px-2"
                 onClick={(e)=>this.handleUpload(e)}>
                   Upload
               </button>               
-            </div>
         </div>
       </Modal>
     )
