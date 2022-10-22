18c33f13d858e8dd7d83ea013d560f5b27076205
Helia <heliasajjadian@gmail.com>
2022-10-21 20:40:02 -0400

    test_routes_deleted

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import testAPI from "./api/routes/test_route.js"
-import voice_flowAPI from "./api/routes/voice_flow_route.js"
 import transcriptAPI from "./api/routes/transcript_route.js"
 
 
@@ -11,10 +9,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use("/api/test", testAPI);
-app.use("/api/VoiceFlow", voice_flowAPI);
 app.use("/api/getTranscript", transcriptAPI);
-app.use("/api/HelloWorld", (req, res) => res.status(200).json({message: "Hello World!"}));
 app.use("*", (req, res) => res.status(404).json({error: "Page not found"}));
 
 export default app;
