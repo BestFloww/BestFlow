68fa6b70dae0da3669138b4fa9327537dbb98214
Helia <heliasajjadian@gmail.com>
2022-10-24 19:40:22 -0400

    whitespace

--- back-end/helpers/transcript_data_formatter.js
+++ back-end/helpers/transcript_data_formatter.js
@@ -16,7 +16,7 @@ export default class TranscriptFormatter extends TranscriptDataInterface{
     }
 
     static #updateContent(entry, content, prev){
-        if(entry["trace_type"] == "text" || entry["trace_type"] == "speak"){ // filter just questions
+        if(entry["trace_type"] == "text" || entry["trace_type"] == "speak"){ // filters just questions
             const tracePayload = JSON.parse(entry["trace_payload"]);
             const id = entry["project_id"];
             const message = tracePayload.payload.message;
@@ -24,26 +24,26 @@ export default class TranscriptFormatter extends TranscriptDataInterface{
             if(prev in content){
                 this.#addChild(content, prev, message, id);
             }
-            prev = (message + id);
+            prev = (message + id); // saves the question as previous to be able to add children
         }
         return prev;
     }
 
     static #addIntent(content, message, id){
-        if(!((message + id) in content)){ // add question to array if does not already exist
-            const intent = {question: message, children: new Map(), totalChildren: 0, projectId: id}; // create intent initializer 
+        if(!((message + id) in content)){
+            const intent = {question: message, children: new Map(), totalChildren: 0, projectId: id}; // creates intent initializer 
             content[(message + id)] = intent;
         }
     }
 
     static #addChild(content, prev, message, id){
         const prevIntent = content[prev];
-        if(prevIntent.projectId == id){ // add child only if it has the same project id as its previous question
+        if(prevIntent.projectId == id){ // adds child only if it has the same project id as its previous question
             if(!(prevIntent.children.has(message))){ 
-                prevIntent.children.set(message, 0); // add child to children if does not already exist
+                prevIntent.children.set(message, 0); 
             }
-            prevIntent.children.set(message, prevIntent.children.get(message) + 1); // update the number of this child
-            prevIntent.totalChildren += 1; // update the number of the question's children
+            prevIntent.children.set(message, prevIntent.children.get(message) + 1); // updates the count of this child
+            prevIntent.totalChildren += 1;
             content[prev] = prevIntent;
         }
     }
