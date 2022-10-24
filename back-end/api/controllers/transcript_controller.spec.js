9fb59ca856e523647ea724289288c735fd0572fe
emilylwan <93227982+emilylwan@users.noreply.github.com>
2022-10-24 16:53:26 -0400

    Fixed test issues for transcript_controller and transcript_data_formatter

--- back-end/api/controllers/transcript_controller.spec.js
+++ back-end/api/controllers/transcript_controller.spec.js
@@ -1,15 +1,17 @@
 import intentDao from "../../dao/intentdao.js";
 import TranscriptController from "./transcript_controller.js";
-import { Intent } from "../../schema/intent-schema.js";
+import {TranscriptFormatter} from "../../helpers/transcript_data_formatter.js";
 
 jest.mock("../../dao/intentdao.js");
 jest.mock("../../schema/intent-schema.js");
+jest.mock("../../helpers/transcript_data_formatter.js")
 
 const mockResponse = () => {
     let res = {}
     res = {
         status: () => res,
-        json: jest.fn().mockReturnValue(res),
+        json: jest.fn().mockImplementation((obj) => obj),
+        error: "error",
     };
     return res
 }
@@ -18,25 +20,21 @@ TranscriptController.setIntentDao(intentDao);
 
 describe("transcriptController", () => {
     it("Should correctly set intentDao and get transcript", async() => {
+        intentDao.getIntent.mockImplementation().mockReturnValue({});
         await TranscriptController.getTranscript({}, mockResponse(), {});
         expect(intentDao.getIntent).toHaveBeenCalled();
     });
 
     it("Should correctly set intentDao and post transcript", async() => {
-        await TranscriptController.postTranscript({}, mockResponse(), {});
+        await TranscriptController.postTranscript({body:{payload : {"a" : "a"}}}, mockResponse(), {});
         expect(intentDao.postIntents).toHaveBeenCalled();
     });
 
-    it("Should correctly throw an error for get transcript", async() => {
-        Intent.find.mockImplementation(() => {throw "error"});
-        const result = await TranscriptController.getTranscript({}, {}, {});
-        expect(result.error).toBe("error");
-    });
-
     it("Should correctly throw an error for post transcript", async() => {
-        Intent.find.mockImplementation(() => {throw "error"});
-        const result = await TranscriptController.postTranscript({},{},{});
-        expect(result.error).toBe("error");
+        intentDao.postIntents.mockImplementation(() => {throw {message : "error"}});
+        const res = mockResponse();
+        const result = await TranscriptController.postTranscript({body:{payload : {"a" : "a"}}}, res,{});
+        expect(res.json).toHaveBeenCalledWith({error : "error"});
     });
 
 });
--- back-end/helpers/transcript_data_formatter.spec.js
+++ back-end/helpers/transcript_data_formatter.spec.js
@@ -1,7 +1,7 @@
-import TranscriptFormatter from "./transcript_data_formatter"
+import {TranscriptFormatter} from "./transcript_data_formatter"
 const globalDatabase = {};
 
-describe("formatTranscipt", () => {
+describe("formatTranscript", () => {
     beforeAll(() => {
         const intent1 = {
           "project_id": "1",
@@ -45,7 +45,7 @@ describe("formatTranscipt", () => {
       it("should throw an error when the transcript does not match specifications ", async() => {
           globalDatabase.rawTranscript.data.push(globalDatabase.badEntry);
           try{
-              const received = await TranscriptFormatter.formatTranscipt(globalDatabase.rawTranscript);
+              const received = await TranscriptFormatter.formatTranscript(globalDatabase.rawTranscript);
           }
           catch(error){
               expect(error.message).toBe("The transcript does not match specifications! Error: Unexpected token u in JSON at position 0");
@@ -54,14 +54,14 @@ describe("formatTranscipt", () => {
 
     it("should return an empty array, when there's no speak or text", async() => {
         globalDatabase.rawTranscript.data.push(globalDatabase.intent1);
-        const received = await TranscriptFormatter.formatTranscipt(globalDatabase.rawTranscript);
+        const received = await TranscriptFormatter.formatTranscript(globalDatabase.rawTranscript);
         expect(received).toEqual([]);
     });
 
 
     it("should return an array containing one Intent initializer, when the type of question is speak, no children", async() => {
         globalDatabase.rawTranscript.data.push(globalDatabase.speak1);
-        const received = await TranscriptFormatter.formatTranscipt(globalDatabase.rawTranscript);
+        const received = await TranscriptFormatter.formatTranscript(globalDatabase.rawTranscript);
         expect(received.length).toBe(1);
         expect(received[0].question).toBe("<voice name=\"Alexa\">\"I want to plan my schedule\" Is this a key location? </voice>");
         expect(received[0].children).toEqual(new Map());
@@ -72,7 +72,7 @@ describe("formatTranscipt", () => {
 
     it("should return an array containing one Intent initializer, when the type of question is text, no children", async() => {
         globalDatabase.rawTranscript.data.push(globalDatabase.text2);
-        const received = await TranscriptFormatter.formatTranscipt(globalDatabase.rawTranscript);
+        const received = await TranscriptFormatter.formatTranscript(globalDatabase.rawTranscript);
         expect(received.length).toBe(1);
         expect(received[0].question).toBe("What would you like to buy?");
         expect(received[0].children).toEqual(new Map());
@@ -83,7 +83,7 @@ describe("formatTranscipt", () => {
     it("should return an array containing two Intent initializers, two different project ids, no children", async() => {
         globalDatabase.rawTranscript.data.push(globalDatabase.speak1);
         globalDatabase.rawTranscript.data.push(globalDatabase.text2);
-        const received = await TranscriptFormatter.formatTranscipt(globalDatabase.rawTranscript);
+        const received = await TranscriptFormatter.formatTranscript(globalDatabase.rawTranscript);
         expect(received.length).toBe(2);
         expect(received[0].children).toEqual(new Map());
         expect(received[0].totalChildren).toBe(0);
@@ -95,7 +95,7 @@ describe("formatTranscipt", () => {
         globalDatabase.rawTranscript.data.push(globalDatabase.text1);
         const children = new Map();
         children.set("What would you like to buy?", 1);
-        const received = await TranscriptFormatter.formatTranscipt(globalDatabase.rawTranscript);
+        const received = await TranscriptFormatter.formatTranscript(globalDatabase.rawTranscript);
         expect(received.length).toBe(2);
         expect(received[0].children).toEqual(children);
         expect(received[0].totalChildren).toBe(1);
@@ -111,7 +111,7 @@ describe("formatTranscipt", () => {
         globalDatabase.rawTranscript.data.push(globalDatabase.text1);
         const children = new Map();
         children.set("What would you like to buy?", 2);
-        const received = await TranscriptFormatter.formatTranscipt(globalDatabase.rawTranscript);
+        const received = await TranscriptFormatter.formatTranscript(globalDatabase.rawTranscript);
         expect(received.length).toBe(3);
         expect(received[0].children).toEqual(children);
         expect(received[0].totalChildren).toBe(2);
