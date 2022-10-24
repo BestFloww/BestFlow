cee434f534700fe2187b23f2f1c1a32c4b07908b
williamyao27 <william.yao.27@gmail.com>
2022-10-23 20:47:27 -0400

    Update transcript_route.js

--- back-end/api/routes/transcript_route.js
+++ back-end/api/routes/transcript_route.js
@@ -5,19 +5,16 @@ import TranscriptFormatter from "../../helpers/transcript_data_formatter.js";
 
 const router = express.Router();
 
-TranscriptController.setIntentDao(intentDao);
-TranscriptController.setTranscriptFormatter(TranscriptFormatter);
-
-getTranscript((req, res, next) => {
-    TranscriptController.get(req, res, next);
+router.route("/").get((req, res, next) => {
+    TranscriptController.setIntentDao(intentDao)
+    TranscriptController.setTranscriptFormatter(TranscriptFormatter)
+    TranscriptController.get(req, res, next)
 })
 
-postTranscript((req, res, next) => {
-    TranscriptController.post(req, res, next);
+router.route("/").post((req, res, next) => {
+    TranscriptController.setIntentDao(intentDao)
+    TranscriptController.setTranscriptFormatter(TranscriptFormatter)
+    TranscriptController.post(req, res, next)
 })
-
-router.route("/").get(TranscriptController.getTranscript);
-
-router.route("/").post(TranscriptController.postTranscript);
 
 export default router;
