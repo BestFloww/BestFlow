50166e4f59b805c7c7debcfb9d34a64dc92817c2
williamyao27 <william.yao.27@gmail.com>
2022-10-25 00:23:25 -0400

    Unbolded BaseButton font

--- front-end/src/components/BaseButton.jsx
+++ front-end/src/components/BaseButton.jsx
@@ -5,7 +5,7 @@ import Icon from './Icon.jsx';
 const BaseButton = (props) => {
     // Change button styling based on whether it is enabled or disabled
     const getButtonStyle = () => {
-        const baseStyling = "bg-blue-300 font-bold rounded-lg shadow-lg py-3 px-6 ";
+        const baseStyling = "bg-blue-300 rounded-lg shadow-lg py-3 px-6 ";
         const disabledStyling = "opacity-50";
         const enabledStyling = "hover:bg-blue-200 active:bg-blue-400";
         return baseStyling + (props.isDisabled ? disabledStyling : enabledStyling);
