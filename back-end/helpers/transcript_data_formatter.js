1fb4b0d67e99082a18404678eec1f52acbfb0923
Helia <heliasajjadian@gmail.com>
2022-10-25 11:32:46 -0400

    update_comments

--- back-end/helpers/transcript_data_formatter.js
+++ back-end/helpers/transcript_data_formatter.js
@@ -5,6 +5,7 @@ export default class TranscriptFormatter extends TranscriptDataInterface{
         let content = {};
         let prev = "";
         try{
+        // makes sure the transcript is in an expected form
             for(const entry of rawTranscript["data"]){
                 prev = this.#updateContent(entry, content, prev);
             }
@@ -16,7 +17,8 @@ export default class TranscriptFormatter extends TranscriptDataInterface{
     }
 
     static #updateContent(entry, content, prev){
-        if(entry["trace_type"] == "text" || entry["trace_type"] == "speak"){ // filters just questions
+        if(entry["trace_type"] == "text" || entry["trace_type"] == "speak"){
+        // filters just questions to show the flow between each question and the following questions
             const tracePayload = JSON.parse(entry["trace_payload"]);
             const id = entry["project_id"];
             const message = tracePayload.payload.message;
@@ -24,25 +26,31 @@ export default class TranscriptFormatter extends TranscriptDataInterface{
             if(prev in content){
                 this.#addChild(content, prev, message, id);
             }
-            prev = (message + id); // saves the question as previous to be able to add children
+            prev = (message + id);
+            // keeps track of a unique string representing this question to add children to it later
         }
         return prev;
     }
 
     static #addIntent(content, message, id){
         if(!((message + id) in content)){
-            const intent = {question: message, children: new Map(), totalChildren: 0, projectId: id}; // creates intent initializer 
+        // adds question only if it's not already existing
+            const intent = {question: message, children: new Map(), totalChildren: 0, projectId: id};
+            // creates default intent object to be passed to an Intent(see intent_scehma)
             content[(message + id)] = intent;
         }
     }
 
     static #addChild(content, prev, message, id){
         const prevIntent = content[prev];
-        if(prevIntent.projectId == id){ // adds child only if it has the same project id as its previous question
+        if(prevIntent.projectId == id){
+        // adds/updates child only if it has the same project id as its previous question
             if(!(prevIntent.children.has(message))){
+            // adds child only if it's not already existing
                 prevIntent.children.set(message, 0);
             }
-            prevIntent.children.set(message, prevIntent.children.get(message) + 1); // updates the count of this child
+            prevIntent.children.set(message, prevIntent.children.get(message) + 1);
+            // updates the count of this child
             prevIntent.totalChildren += 1;
             content[prev] = prevIntent;
         }
