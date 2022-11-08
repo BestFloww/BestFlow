1a1c4f6f652c494fb40d547e4852ff9852247ec9
emilylwan <93227982+emilylwan@users.noreply.github.com>
2022-11-08 18:55:04 -0500

    No intent needed

--- front-end/src/components/helpers/ExampleTranscript.js
+++ front-end/src/components/helpers/ExampleTranscript.js
@@ -5,9 +5,4 @@ export const exampleTranscript = {"data": [
             "trace_payload": "{\"type\":\"speak\",\"payload\":{" +
                 "\"type\":\"message\":\"<voice name=\\\"[AnyVoiceName]\\\">[INSERT CHATBOT MESSAGE]</voice>\"}}"
         },
-        {
-            "project_id": "[INSERT UNIQUE ID]",
-            "trace_type": "intent",
-            "trace_payload": "{\"type\":\"intent\",\"ELICIT\":false,\"payload\":{" +
-                "\"query\":\"[INSERT QUERY]\",\"intent\":{\"name\":\"[INSERT INTENT]\"},\"confidence\":1}}"
-        }]};
+    ]};
