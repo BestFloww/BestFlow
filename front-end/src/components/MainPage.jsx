9b37984905f6bbcd22032e2a46b622ab6fe29039
emilylwan <93227982+emilylwan@users.noreply.github.com>
2022-11-08 10:25:43 -0500

    Added a download transcript template button

--- front-end/src/components/MainPage.jsx
+++ front-end/src/components/MainPage.jsx
@@ -19,6 +19,42 @@ class MainPage extends Component {
     store.dispatch(openAnalysisPage());
   }
 
+  downloadTemplateJSON = () => {
+    const element = document.createElement("a");
+    const data = {"data": [
+        {
+          "": "23606",
+          "project_id": "1",
+          "turn_id": "dd8e6fee-2329-4470-9cc0-0c50f3d40313",
+          "ts": "2022-04-20 18:01:59.863 UTC",
+          "session_id": "c1ade6a3e2d0171d8813a2279f69a97171258a6c341547919fc0a3f95909b744",
+          "platform": "prototype",
+          "interaction_metadata": "{\"end\":false,\"locale\":\"en-US\"}",
+          "trace_format": "trace",
+          "trace_type": "speak",
+          "trace_payload": "{\"type\":\"speak\",\"payload\":{\"type\":\"message\",\"message\":\"<voice name=\\\"Alexa\\\">Rerouting. ETA for first delivery is 6:20 PM.</voice>\"}}"
+        },
+        {
+          "": "23622",
+          "project_id": "1",
+          "turn_id": "74d52b1e-1af0-480e-8f2a-2ec2c6df7c8b",
+          "ts": "2022-04-20 18:01:53.779 UTC",
+          "session_id": "c1ade6a3e2d0171d8813a2279f69a97171258a6c341547919fc0a3f95909b744",
+          "platform": "prototype",
+          "interaction_metadata": "{\"end\":false,\"locale\":\"en-US\"}",
+          "trace_format": "request",
+          "trace_type": "intent",
+          "trace_payload": "{\"type\":\"intent\",\"ELICIT\":false,\"payload\":{\"query\":\"complete the order\",\"intent\":{\"name\":\"finish_order\"},\"confidence\":1}}"
+        }]};
+    const file = new Blob([JSON.stringify(data)], {
+      type: "application/json",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = "template-transcript-JSON.json"
+    document.body.appendChild(element);
+    element.click();
+  }
+
   render() {
     let isTranscriptUploaded = this.props.isTranscriptUploaded;
     return(
@@ -47,6 +83,13 @@ class MainPage extends Component {
               text="View Analysis"
               size="lg"
               isDisabled={!isTranscriptUploaded}
+            />
+          </div>
+          <div className="justify-center flex">
+            <BaseButton
+                click={this.downloadTemplateJSON}
+                text="Download Template JSON"
+                size="lg"
             />
           </div>
         </div>
