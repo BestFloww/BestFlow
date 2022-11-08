80ec28c18bfa006ff27e3f04e598ddeb356252db
emilylwan <93227982+emilylwan@users.noreply.github.com>
2022-11-08 18:07:03 -0500

    Simplified transcript template

--- front-end/src/components/MainPage.jsx
+++ front-end/src/components/MainPage.jsx
@@ -5,6 +5,7 @@ import { openAnalysisPage } from "../store/switchPageSlice.js";
 import BaseButton from "./BaseButton.jsx";
 import TranscriptUploadModal from "./TranscriptUploadModal.jsx";
 import Title from "./icons/title.jsx";
+import {exampleTranscript} from "./helpers/ExampleTranscript.js";
 
 class MainPage extends Component {
   state = {
@@ -21,32 +22,7 @@ class MainPage extends Component {
 
   downloadTranscriptTemplate = () => {
     const element = document.createElement("a");
-    const data = {"data": [
-        {
-          "": "23606",
-          "project_id": "1",
-          "turn_id": "dd8e6fee-2329-4470-9cc0-0c50f3d40313",
-          "ts": "2022-04-20 18:01:59.863 UTC",
-          "session_id": "c1ade6a3e2d0171d8813a2279f69a97171258a6c341547919fc0a3f95909b744",
-          "platform": "prototype",
-          "interaction_metadata": "{\"end\":false,\"locale\":\"en-US\"}",
-          "trace_format": "trace",
-          "trace_type": "speak",
-          "trace_payload": "{\"type\":\"speak\",\"payload\":{\"type\":\"message\",\"message\":\"<voice name=\\\"Alexa\\\">Rerouting. ETA for first delivery is 6:20 PM.</voice>\"}}"
-        },
-        {
-          "": "23622",
-          "project_id": "1",
-          "turn_id": "74d52b1e-1af0-480e-8f2a-2ec2c6df7c8b",
-          "ts": "2022-04-20 18:01:53.779 UTC",
-          "session_id": "c1ade6a3e2d0171d8813a2279f69a97171258a6c341547919fc0a3f95909b744",
-          "platform": "prototype",
-          "interaction_metadata": "{\"end\":false,\"locale\":\"en-US\"}",
-          "trace_format": "request",
-          "trace_type": "intent",
-          "trace_payload": "{\"type\":\"intent\",\"ELICIT\":false,\"payload\":{\"query\":\"complete the order\",\"intent\":{\"name\":\"finish_order\"},\"confidence\":1}}"
-        }]};
-    const file = new Blob([JSON.stringify(data)], {
+    const file = new Blob([JSON.stringify(exampleTranscript)], {
       type: "application/json",
     });
     element.href = URL.createObjectURL(file);
---
+++ front-end/src/components/helpers/ExampleTranscript.js
@@ -0,0 +1,13 @@
+export const exampleTranscript = {"data": [
+        {
+            "project_id": "Your unique project id",
+            "trace_type": "speak",
+            "trace_payload": "{\"type\":\"speak\",\"payload\":{" +
+                "\"type\":\"message\":\"<voice name=\\\"AnyVoiceName\\\">Insert chatbot message.</voice>\"}}"
+        },
+        {
+            "project_id": "Your unique project id",
+            "trace_type": "intent",
+            "trace_payload": "{\"type\":\"intent\",\"ELICIT\":false,\"payload\":{" +
+                "\"query\":\"unique query\",\"intent\":{\"name\":\"unique intent\"},\"confidence\":1}}"
+        }]};
