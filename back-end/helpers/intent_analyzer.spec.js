00a6a6a94e0d8d0613aeb8316a6e421c5cd684c6
Kaylee Chan <kayleel.chan@gmail.com>
2022-11-09 15:30:05 -0500

    Adding multiple children test, changing file name, and adding semicolons

--- back-end/helpers/intent_analyzer.spec.js
+++ back-end/helpers/intent_analyzer.spec.js
@@ -1,8 +1,8 @@
-import TestIntentGenerator from "./test-helpers/testIntentGenerator.js";
+import TestIntentFactory from "./test-helpers/testIntentFactory.js";
 import IntentAnalyzer from "./intent_analyzer.js"
 
 let analyzer;
-let generator;
+let factory;
 
 const getPercentages = ((children, total_children) => {
   const percentageMap = {}
@@ -15,24 +15,24 @@ const getPercentages = ((children, total_children) => {
 describe("intentAnalyzer", () => {
   beforeEach(() => {
     analyzer = new IntentAnalyzer();
-    generator = new TestIntentGenerator();
+    factory = new TestIntentFactory();
   });
 
-  it("Should return an empty array if given no model", async() => {
+  it("Should return an empty array if given no model", () => {
     expect(analyzer.analyzeIntents([])).toStrictEqual([]);
   });
 
-  it("Should properly format one intent", async() => {
+  it("Should properly format one intent", () => {
     const fakeMap = new Map();
     fakeMap.set("Intent 1-DOT-", 1);
-    const input = generator.generateModel("Question 1-DOT-", fakeMap, 1, 1);
-    input.getPercentages.mockImplementation(() => {return getPercentages(input.children, input.total_children)})
-    const output = generator.generateAnalyzedIntent("Question 1.", {"Intent 1.": 1.00});
+    const input = factory.generateModel("Question 1-DOT-", fakeMap, 1, 1);
+    input.getPercentages.mockImplementation(() => {return getPercentages(input.children, input.total_children)});
+    const output = factory.generateAnalyzedIntent("Question 1.", {"Intent 1.": 1.00});
 
     expect(analyzer.analyzeIntents([input])).toStrictEqual([output]);
   });
 
-  it("Should properly format three intents", async() => {
+  it("Should properly format three intents", () => {
     const fakeMap1 = new Map();
     fakeMap1.set("Intent 1-DOT-", 7);
     const fakeMap2 = new Map();
@@ -40,20 +40,33 @@ describe("intentAnalyzer", () => {
     const fakeMap3 = new Map();
     fakeMap3.set("Intent 3-DOT-", 1);
 
-    const input1 = generator.generateModel("Question 1-DOT-", fakeMap1, 10, 1);
-    const input2 = generator.generateModel("Question 2-DOT-", fakeMap2, 10, 1);
-    const input3 = generator.generateModel("Question 3-DOT-", fakeMap3, 10, 1);
-    input1.getPercentages.mockImplementation(() => {return getPercentages(input1.children, input1.total_children)})
-    input2.getPercentages.mockImplementation(() => {return getPercentages(input2.children, input2.total_children)})
-    input3.getPercentages.mockImplementation(() => {return getPercentages(input3.children, input3.total_children)})
+    const input1 = factory.generateModel("Question 1-DOT-", fakeMap1, 7, 1);
+    const input2 = factory.generateModel("Question 2-DOT-", fakeMap2, 2, 1);
+    const input3 = factory.generateModel("Question 3-DOT-", fakeMap3, 1, 1);
+    input1.getPercentages.mockImplementation(() => {return getPercentages(input1.children, input1.total_children)});
+    input2.getPercentages.mockImplementation(() => {return getPercentages(input2.children, input2.total_children)});
+    input3.getPercentages.mockImplementation(() => {return getPercentages(input3.children, input3.total_children)});
 
-    const output = generator.generateMultipleIntents([["Question 1.", {"Intent 1.": 0.70}], ["Question 2.", {"Intent 2.": 0.20}], ["Question 3.", {"Intent 3.": 0.10}]]);
+    const output = factory.generateMultipleIntents([["Question 1.", {"Intent 1.": 1.00}], ["Question 2.", {"Intent 2.": 1.00}], ["Question 3.", {"Intent 3.": 1.00}]]);
     expect(analyzer.analyzeIntents([input1, input2, input3])).toStrictEqual(output);
   });
 
-  it("Should properly format question with no intents", async() => {
-    const input = generator.generateModel("Question 1-DOT-", {}, 0, 1);
-    const output = generator.generateAnalyzedIntent("Question 1.", {"No intents found.": 0});
+  it("Should properly format question with no intents", () => {
+    const input = factory.generateModel("Question 1-DOT-", {}, 0, 1);
+    const output = factory.generateAnalyzedIntent("Question 1.", {"No intents found.": 0});
     expect(analyzer.analyzeIntents([input])).toStrictEqual([output]);
   });
+
+  it("Should properly format question with multiple children", () => {
+    const fakeMap = new Map();
+    fakeMap.set("Intent 1-DOT-", 7);
+    fakeMap.set("Intent 2-DOT-", 2);
+    fakeMap.set("Intent 3-DOT-", 1);
+
+    const input = factory.generateModel("Question 1-DOT-", fakeMap, 10, 1);
+    input.getPercentages.mockImplementation(() => {return getPercentages(input.children, input.total_children)});
+    const output = factory.generateAnalyzedIntent("Question 1.", {"Intent 1.": 0.70, "Intent 2.": 0.20, "Intent 3.": 0.10});
+
+    expect(analyzer.analyzeIntents([input])).toStrictEqual([output]);
+  })
 })
