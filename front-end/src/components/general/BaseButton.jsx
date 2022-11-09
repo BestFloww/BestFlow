7f848efbc0a93b12d12d8f65fe7a0835a05ce116
WeissSa <WeissBsamuel@gmail.com>
2022-11-08 22:05:00 -0500

    removed curly fries

--- front-end/src/components/general/BaseButton.jsx
+++ front-end/src/components/general/BaseButton.jsx
@@ -36,7 +36,7 @@ const BaseButton = (props) => {
                 aria-label={props.label}
                 data-testid="custom-button"
             >
-                <div className={"flex items-center space-x-2"}>
+                <div className="flex items-center space-x-2">
                     {props.icon && <div className="mx-auto"><Icon icon={props.icon}/></div>}
                     {props.text && <div className="mx-auto">{ props.text }</div>}
                 </div>
