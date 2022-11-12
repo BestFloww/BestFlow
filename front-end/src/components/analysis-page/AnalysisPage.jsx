329790701cdbd8387707a17da88994f8857dbade
Kaylee Chan <kayleel.chan@gmail.com>
2022-11-12 18:47:05 -0500

    a11y and tooltip additions

--- front-end/src/components/analysis-page/AnalysisPage.jsx
+++ front-end/src/components/analysis-page/AnalysisPage.jsx
@@ -69,8 +69,14 @@ class AnalysisPage extends Component {
         return (
             <div className="AnalysisPage bg-purple-100 flex h-screen" data-testid="analysis-page">
               <div className="flex gap-y-10 w-full flex-col h-full">
-                <div className="justify-left flex cursor-pointer m-5 w-[5em]"
+                <div /*second line of className is tooltip styling*/
+                  className="justify-left relative text-center text-sm
+                    before:z-10 before:absolute before:-right-5 before:top-1/2 before:w-max before:max-w-xs before:translate-x-full before:-translate-y-1/2 before:rounded-md before:bg-gray before:px-3 before:py-2 before:text-off-white before:invisible before:content-[attr(tooltip)] after:z-10 after:absolute after:-right-[0.8rem] after:top-1/2 after:h-0 after:w-0 after:translate-x-2 after:-translate-y-1/2 after:border-8 after:border-r-gray after:border-l-transparent after:border-b-transparent after:border-t-transparent after:invisible hover:before:visible hover:after:visible
+                    cursor-pointer m-5 w-[5em]"
+                  role="button"
                   onClick={this.openMainPage}
+                  aria-label="Home Button"
+                  tooltip="Click here to return to homepage"
                   data-testid="logo-button"
                 >
                   <Logo />
