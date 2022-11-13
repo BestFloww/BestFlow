0bccacf52f4a1597ade119e5bdb70119aa6a09a7
Kaylee Chan <kayleel.chan@gmail.com>
2022-11-13 11:49:23 -0500

    no async

--- front-end/src/App.test.js
+++ front-end/src/App.test.js
@@ -31,7 +31,7 @@ describe('App', () => {
         expect(screen.queryByTestId('main-page')).not.toBeInTheDocument();
     });
 
-    it('should display MainPage when logo button is pressed on AnalysisPage', async() => {
+    it('should display MainPage when logo button is pressed on AnalysisPage', () => {
         renderComponent();
         userEvent.click(screen.getByTestId('logo-button'));
         expect(screen.getByTestId('main-page')).toBeInTheDocument();
