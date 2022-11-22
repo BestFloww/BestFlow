f68ef3ecbac9afe2da5937d61c091738760ba587
emilylwan <93227982+emilylwan@users.noreply.github.com>
2022-11-22 16:32:29 -0500

    More tests

--- back-end/helpers/get/transcript_data_grouper.spec.js
+++ back-end/helpers/get/transcript_data_grouper.spec.js
@@ -185,8 +185,9 @@ describe("TranscriptDataGrouper", () => {
         });
 
         it("should correctly merge 3 similar intents", async() => {
-            /* TODO: Test is currently broken, it is changing intents which causes a map expectation not being fulfilled
-            / Tried changing the filter to base intents but that changes the deep copy of this test
+            /* TODO: Test is currently broken
+            / Currently receiving false but need to check reasons for why it is not computing properly
+            / Probably in recalculation step where children is not defined
             */
 
             const alreadyProcessedIntents = intents.slice(0, 1);
@@ -216,5 +217,181 @@ describe("TranscriptDataGrouper", () => {
             expect(intents[0].children["c"]).toEqual(22);
             expect(intents[0].children["b"]).toEqual(44);
         });
+    });
+    describe("when intents are similar but separated", () => {
+        // Create fake intents where some should be merged, and some should not be in different order
+        const baseFakeIntents = [
+            {
+                question: "hi how are you I am Sam",
+                children: mapMaker({
+                    "h": 1,
+                    "b": 2,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "hi how are you I am Bob",
+                children: mapMaker({
+                    "h": 1,
+                    "c": 2,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "hi how are you I am Tom",
+                children: mapMaker({
+                    "h": 1,
+                    "b": 2,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "hi how are you I am Mr Bob",
+                children: mapMaker({
+                    "h": 1,
+                    "b": 2,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "This is not similar",
+                children: mapMaker({
+                    "h": 1,
+                    "b": 2,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "This is not similar because it is far too long long long long long long",
+                children: mapMaker({
+                    "h": 1,
+                    "b": 2,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "hi how are you I am David",
+                children: mapMaker({
+                    "h": 2,
+                    "a": 1,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "These are some very similar sentences following this one here",
+                children: mapMaker({
+                    "d": 2,
+                    "h": 1,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "These are some not very same sentence for the next one",
+                children: mapMaker({
+                    "f": 1,
+                    "a": 2,
+                }),
+                total_children: 3,
+                project_id: 1,
+            },
+            {
+                question: "These are some almost similar sentences following this one here",
+                children: mapMaker({
+                    "d": 1,
+                    "a": 2,
+                    "b": 2,
+                }),
+                total_children: 5,
+                project_id: 1,
+            },
+            {
+                question: "These are some very similar sentences following this one",
+                children: mapMaker({
+                    "h": 1,
+                    "c": 1,
+                }),
+                total_children: 2,
+                project_id: 1,
+            },
+        ]
+        beforeEach(() => {
+            // create a new deep copy of the array each test to prevent bleeding
+            intents = [];
+            for (const fake of baseFakeIntents) {
+                // make a deep copy of the map
+                intents.push({
+                    ...fake,
+                    children: new Map(fake.children),
+                });
+            }
+            jest.spyOn(fakeDao, "getIntent").mockImplementation((query) => {
+                const result = intents.filter((intent) => {
+                    return intent.question === query.question && intent.project_id === query.project_id;
+                });
+                const newChildren = new Map(JSON.parse(JSON.stringify(Array.from(result[0].children))))
+                const deepIntent = JSON.parse(JSON.stringify(result[0]))
+                deepIntent.children = newChildren
+                return [deepIntent];
+            });
+        });
+
+        it("should correctly merge 4 similar intents", async() => {
+            // TODO: Test is currently broken
+
+            const alreadyProcessedIntents = intents.slice(0, 1);
+            const grouper = new TranscriptDataGrouper(alreadyProcessedIntents, 1);
+
+            for (const similarIntent of intents.slice(1, 3)) {
+                const result = await grouper.group(similarIntent);
+                expect(fakeDao.getIntent).toHaveBeenCalled();
+                expect(result).toBe(true);
+            }
+
+            const lastIntent = intents[6];
+
+            const result = await grouper.group(lastIntent)
+            expect(fakeDao.getIntent).toHaveBeenCalled();
+            expect(result).toBe(true);
+
+            expect(intents[0].children["h"]).toEqual(42);
+            expect(intents[0].children["c"]).toEqual(17);
+            expect(intents[0].children["b"]).toEqual(33);
+            expect(intents[0].children["a"]).toEqual(8);
+        });
+
+        it("should correctly merge 3 similar intents in different places", async() => {
+            // TODO: Test is currently broken
+
+            const alreadyProcessedIntents = intents.slice(0, 8);
+            const grouper = new TranscriptDataGrouper(alreadyProcessedIntents, 1);
+
+            const firstIntent = intents[8];
+            const result = await grouper.group(firstIntent)
+
+            expect(fakeDao.getIntent).toHaveBeenCalled();
+            expect(result).toBe(false);
+
+            for (const similarIntent of intents.slice(9, 11)) {
+                const result = await grouper.group(similarIntent);
+                expect(fakeDao.getIntent).toHaveBeenCalled();
+                expect(result).toBe(true);
+            }
+
+            expect(intents[0].children["h"]).toEqual(20);
+            expect(intents[0].children["d"]).toEqual(30);
+            expect(intents[0].children["b"]).toEqual(20);
+            expect(intents[0].children["a"]).toEqual(20);
+            expect(intents[0].children["c"]).toEqual(10);
+        });
+
+        
     });
 });
