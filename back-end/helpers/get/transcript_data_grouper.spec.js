40cc20696002d1f85d4956451f7100a8f52bca26
emilylwan <93227982+emilylwan@users.noreply.github.com>
2022-11-24 09:47:39 -0500

    Alter tests

--- back-end/helpers/get/transcript_data_grouper.spec.js
+++ back-end/helpers/get/transcript_data_grouper.spec.js
@@ -101,132 +101,6 @@ describe("TranscriptDataGrouper", () => {
                     "h": 1,
                     "b": 2,
                 }),
-                total_children:3,
-                project_id: 1,
-            },
-            {
-                question: "hi how are you I am Bob",
-                children: mapMaker({
-                    "h": 1,
-                    "c": 2,
-                }),
-                total_children:3,
-                project_id: 1,
-            },
-            {
-                question: "hi how are you I am Tom",
-                children: mapMaker({
-                    "h": 1,
-                    "b": 2,
-                }),
-                total_children:3,
-                project_id: 1,
-            },
-            {
-                question: "hi how are you I am Mr Bob",
-                children: mapMaker({
-                    "h": 1,
-                    "b": 2,
-                }),
-                total_children:3,
-                project_id: 1,
-            },
-            {
-                question: "This is not similar",
-                children: mapMaker({
-                    "h": 1,
-                    "b": 2,
-                }),
-                total_children:3,
-                project_id: 1,
-            },
-            {
-                question: "This is not similar because it is far too long long long long long long",
-                children: mapMaker({
-                    "h": 1,
-                    "b": 2,
-                }),
-                total_children:3,
-                project_id: 1,
-            },
-        ]
-        beforeEach(() => {
-            // create a new deep copy of the array each test to prevent bleeding
-            intents = [];
-            for (const fake of baseFakeIntents) {
-                // make a deep copy of the map
-                intents.push({
-                    ...fake,
-                    children: new Map(fake.children),
-                });
-            }
-            jest.spyOn(fakeDao, "getIntent").mockImplementation((query) => {
-                const result = intents.filter((intent) => {
-                    return intent.question === query.question && intent.project_id === query.project_id;
-                });
-                const newChildren = new Map(JSON.parse(JSON.stringify(Array.from(result[0].children))))
-                const deepIntent = JSON.parse(JSON.stringify(result[0]))
-                deepIntent.children = newChildren
-                return [deepIntent];
-            });
-        });
-
-        it("should correctly merge 2 similar intents", async() => {
-            const alreadyProcessedIntents = intents.slice(0, 1);
-            const similarIntent = intents[1];
-            const grouper = new TranscriptDataGrouper(alreadyProcessedIntents, 1);
-            
-            const result = await grouper.group(similarIntent);
-            expect(fakeDao.getIntent).toHaveBeenCalled();
-            expect(intents[0].children["h"]).toEqual(33);
-            expect(intents[0].children["c"]).toEqual(33);
-            expect(intents[0].children["b"]).toEqual(33);
-            expect(result).toBe(true);
-        });
-
-        it("should correctly merge 3 similar intents", async() => {
-            /* TODO: Test is currently broken
-            / Currently receiving false but need to check reasons for why it is not computing properly
-            / Probably in recalculation step where children is not defined
-            */
-
-            const alreadyProcessedIntents = intents.slice(0, 1);
-            const grouper = new TranscriptDataGrouper(alreadyProcessedIntents, 1);
-
-            for (const similarIntent of intents.slice(1, 3)) {
-                const result = await grouper.group(similarIntent);
-                expect(fakeDao.getIntent).toHaveBeenCalled();
-                expect(result).toBe(true);
-            }
-            expect(intents[0].children["h"]).toEqual(33);
-            expect(intents[0].children["c"]).toEqual(22);
-            expect(intents[0].children["b"]).toEqual(44);
-        });
-
-        it("should correctly merge first 3 similar intents and not the rest", async() => {
-            // TODO: Test is also broken
-
-            const alreadyProcessedIntents = intents.slice(0, 1);
-            const grouper = new TranscriptDataGrouper(alreadyProcessedIntents, 1);
-
-            for (const intent of intents.slice(1)) {
-                await grouper.group(intent);
-                expect(fakeDao.getIntent).toHaveBeenCalled();
-            }
-            expect(intents[0].children["h"]).toEqual(33);
-            expect(intents[0].children["c"]).toEqual(22);
-            expect(intents[0].children["b"]).toEqual(44);
-        });
-    });
-    describe("when intents are similar but separated", () => {
-        // Create fake intents where some should be merged, and some should not be in different order
-        const baseFakeIntents = [
-            {
-                question: "hi how are you I am Sam",
-                children: mapMaker({
-                    "h": 1,
-                    "b": 2,
-                }),
                 total_children: 3,
                 project_id: 1,
             },
@@ -343,6 +217,52 @@ describe("TranscriptDataGrouper", () => {
             });
         });
 
+        it("should correctly merge 2 similar intents", async() => {
+            const alreadyProcessedIntents = intents.slice(0, 1);
+            const similarIntent = intents[1];
+            const grouper = new TranscriptDataGrouper(alreadyProcessedIntents, 1);
+            
+            const result = await grouper.group(similarIntent);
+            expect(fakeDao.getIntent).toHaveBeenCalled();
+            expect(intents[0].children["h"]).toEqual(33);
+            expect(intents[0].children["c"]).toEqual(33);
+            expect(intents[0].children["b"]).toEqual(33);
+            expect(result).toBe(true);
+        });
+
+        it("should correctly merge 3 similar intents", async() => {
+            /* TODO: Test is currently broken
+            / Currently receiving false but need to check reasons for why it is not computing properly
+            / Probably in recalculation step where children is not defined
+            */
+
+            const alreadyProcessedIntents = intents.slice(0, 1);
+            const grouper = new TranscriptDataGrouper(alreadyProcessedIntents, 1);
+
+            for (const similarIntent of intents.slice(1, 3)) {
+                const result = await grouper.group(similarIntent);
+                expect(fakeDao.getIntent).toHaveBeenCalled();
+                expect(result).toBe(true);
+            }
+            expect(intents[0].children["h"]).toEqual(33);
+            expect(intents[0].children["c"]).toEqual(22);
+            expect(intents[0].children["b"]).toEqual(44);
+        });
+
+        it("should correctly merge first 3 similar intents and not the rest", async() => {
+            // TODO: Test is also broken
+
+            const alreadyProcessedIntents = intents.slice(0, 1);
+            const grouper = new TranscriptDataGrouper(alreadyProcessedIntents, 1);
+
+            for (const intent of intents.slice(1)) {
+                await grouper.group(intent);
+                expect(fakeDao.getIntent).toHaveBeenCalled();
+            }
+            expect(intents[0].children["h"]).toEqual(33);
+            expect(intents[0].children["c"]).toEqual(22);
+            expect(intents[0].children["b"]).toEqual(44);
+        });
         it("should correctly merge 4 similar intents", async() => {
             // TODO: Test is currently broken
 
