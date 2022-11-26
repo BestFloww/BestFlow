2a61d914721e38b76731dc7df60d0bb468bfe47f
Kaylee Chan <kayleel.chan@gmail.com>
2022-11-26 17:52:26 -0500

    Added custom scrollbar and small fe changes

--- front-end/src/components/main-page/TranscriptDescription.jsx
+++ front-end/src/components/main-page/TranscriptDescription.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {exampleTranscript} from "../helpers/ExampleTranscript.js"; 
+import {exampleTranscript} from "../helpers/ExampleTranscript.js";
 
 class TranscriptDescription extends Component {
 	render() {
