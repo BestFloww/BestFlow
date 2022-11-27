3015b77bc885a16629820bbd178624adef6c14be
emilylwan <93227982+emilylwan@users.noreply.github.com>
2022-11-27 16:41:15 -0500

    Undo changes from a different branch

--- front-end/src/components/analysis-page/AnalysisPage.jsx
+++ front-end/src/components/analysis-page/AnalysisPage.jsx
@@ -50,18 +50,6 @@ export class AnalysisPage extends Component {
           >
             <div className="flex gap-y-10 w-full flex-col h-full">
               <div className= "w-full bg-gray-100 pb-2">
-                <div
-                  className="absolute m-2 mx-auto px-3" data-testid="arrow-right"
-                >
-                  <BaseButton
-                    click={this.openIntentMenu}
-                    label="Open intent menu"
-                    icon={{
-                      name: "hamburger",
-                      size: "40",
-                          }}
-                      />
-                </div>
                 <div /* first line of className is tooltip styling */
                   className="relative before:z-10 before:absolute before:left-1/2 before:-bottom-3 before:w-max before:max-w-xs before:-translate-x-1/2 before:translate-y-full before:rounded-lg before:bg-gray-200 before:px-2 before:py-1.5 before:text-off-white before:invisible before:content-[attr(tooltip)] after:z-10 after:absolute after:left-1/2 after:-bottom-3 after:h-0 after:w-0 after:-translate-x-1/2 after:border-8 after:border-b-gray-200 after:border-l-transparent after:border-t-transparent after:border-r-transparent after:invisible hover:before:visible hover:after:visible
                   absolute cursor-pointer m-3 sm:w-[11em] mx-auto"
@@ -81,6 +69,18 @@ export class AnalysisPage extends Component {
                 />
               </div>
             </div>
+          </div>
+          <div /*TEMPORARY SIDEBAR CONTROL BUTTON!*/
+              className="fixed right-20 sm:right-40 bottom-1 sm:bottom-8" data-testid="arrow-right"
+          >
+              <BaseButton
+                  click={this.openIntentMenu}
+                  label="Open intent menu"
+                  icon={{
+                      name: "hamburger",
+                      size: "40",
+                  }}
+              />
           </div>
         </div>
       );
